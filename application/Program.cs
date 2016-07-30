fdd28b7d65d4bfadf11959e5c0374a1681bd8210
Micah Zoltu <micah@zoltu.net>
2016-07-30 11:27:46 -0700

    Initial project setup.

---
+++ application/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace Zoltu.BagsAmazonSynchronizer
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var configuration = new ConfigurationBuilder();
+		}
+	}
+}
---
+++ tests/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace Zoltu.BagsAmazonSynchronizer.Tests
+{
+	public class Tests
+	{
+		[Fact]
+		public void main_does_not_throw()
+		{
+			Program.Main(new String[]{});
+			Assert.True(true);
+		}
+	}
+}
