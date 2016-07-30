2b405f5402dff2813bc506df63626272c06f735c
Micah Zoltu <micah@zoltu.net>
2016-07-30 12:02:26 -0700

    Creates Visual Studio files.
    Also sets up configuration.

--- application/Program.cs
+++ application/Program.cs
@@ -5,9 +5,41 @@ namespace Zoltu.BagsAmazonSynchronizer
 {
 	public class Program
 	{
+		public class Configuration
+		{
+			public readonly String AmazonAssociateTag;
+			public readonly String AmazonAccessKey;
+			public readonly String AmazonSecretKey;
+			public readonly String SqlServerConnectionString;
+
+			public Configuration()
+			{
+				var configuration = new ConfigurationBuilder()
+				.AddEnvironmentVariables()
+				.AddUserSecrets("bags-amazon-synchronization")
+				.Build();
+
+				AmazonAssociateTag = configuration["AmazonAssociateTag"];
+				if (AmazonAssociateTag == null)
+					throw new Exception("Required configuration option AmazonAssociateTag not found.");
+
+				AmazonAccessKey = configuration["AmazonAccessKeyId"];
+				if (AmazonAccessKey == null)
+					throw new Exception("Required configuration option AmazonAccessKeyId not found.");
+
+				AmazonSecretKey = configuration["AmazonSecretAccessKey"];
+				if (AmazonSecretKey == null)
+					throw new Exception("Required configuration option AmazonSecretAccessKey not found.");
+
+				SqlServerConnectionString = configuration["SqlServerConnectionString"];
+				if (SqlServerConnectionString == null)
+					throw new Exception("Required configuration option SqlServerConnectionString not found.");
+			}
+		}
+
 		public static void Main(string[] args)
 		{
-			var configuration = new ConfigurationBuilder();
+			var configuration = new Configuration();
 		}
 	}
 }
