1fc05a1b8051d06b9623cebdb946f1d0c74a927e
Micah Zoltu <micah@zoltu.net>
2016-08-08 20:41:35 -0700

    Change to always run.

--- application/Program.cs
+++ application/Program.cs
@@ -12,7 +12,7 @@ namespace application
 
 		    using (var amz = new AmazonSynchronizer(new Configuration()))
 		    {
-                amz.WithInterval(TimeSpan.FromMinutes(5))
+                amz.WithInterval(TimeSpan.FromSeconds(1))
                     .StopWhen((obj) => false)
                     .SetLogger(new SimpleFileLogger())
                     .Start(CancellationToken.None)
